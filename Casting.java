e669c5245c97c909bbb9e9a110420b9d67dbd613
Ravi sahani <108450291+Ravicsecap@users.noreply.github.com>
2022-07-05 11:55:41 +0530

    casting.java

---
+++ Casting.java
@@ -0,0 +1,14 @@
+package javafullcourse;
+
+public class Casting {
+
+	public static void main(String[] args) {
+	char letter='b';
+	 int ravi=letter;
+	 char alp=(char)ravi;
+	 System.out.println("value of int value of letter:" +ravi );
+	 System.out.println("value of char value of letter:" +letter);
+
+	}
+
+}
